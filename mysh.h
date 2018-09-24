1939a2dafdc5590f3e3929d216b80b485158ad56
Samantha Foley <ssfoley@cs.uwlax.edu>
2018-09-24 21:04:46 +0000

    mysh.h edited online with Bitbucket

--- mysh.h
+++ mysh.h
@@ -1,5 +1,5 @@
 /*
- * Josh Hursey, Samantha Foley, [YOUR NAME HERE]
+ * [YOUR NAME HERE]
  *
  * CS441/541: Project 3
  *
@@ -24,25 +24,26 @@
 /******************************
  * Structures
  ******************************/
+/*
+ * A job struct.  Feel free to change as needed.
+ */
+struct job_t {
+    char * full_command;
+    int argc;
+    char **argv;
+    int is_background;
+    char * binary;
+};
+typedef struct job_t job_t;
 
 /******************************
  * Global Variables
  ******************************/
-/*
- * Debugging toggle
- */
-int is_debug = FALSE;
-
+ 
 /*
  * Interactive or batch mode
  */
 int is_batch = FALSE;
-
-/*
- * Batch file names
- */
-int num_batch_files = 0;
-char **batch_files = NULL;
 
 /*
  * Counts
@@ -51,12 +52,6 @@ int total_jobs_display_ctr = 0;
 int total_jobs    = 0;
 int total_jobs_bg = 0;
 int total_history = 0;
-
-/*
- * If we are exiting
- */
-int exiting = FALSE;
-
 
 /******************************
  * Function declarations
@@ -97,30 +92,6 @@ int batch_mode(void);
  *   Negative value on error
  */
 int interactive_mode(void);
-
-/*
- * Split possible multiple jobs on a command line, then call parse_and_run()
- *
- * Parameters:
- *  command : command line string (may contain multiple jobs)
- *
- * Returns:
- *   0 on success
- *   Negative value on error
- */
-int split_parse_and_run(char * command);
-
-/*
- * Parse and execute a single job given to the prompt.
- *
- * Parameters:
- *   loc_job : job to execute
- *
- * Returns:
- *   0 on success
- *   Negative value on error
- */
-int parse_and_run(job_t * loc_job);
 
 /*
  * Launch a job
